28dbf75401d8164c1e1b2ff0e70867cae3315e78
pavanakshaypappala-gif <pavanakshaypappala@gmail.com>
2026-08-13 11:25:03 +0530

    Time: 0 ms (100%), Space: 13.4 MB (36.81%) - LeetHub

---
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def search(self, nums, target):
+        i = 0
+        j = len(nums)-1
+        while i<=j:
+            mid = (i+j)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<=target:
+                i = mid+1
+            else:
+                j = mid-1
+        return -1
+        
+        
